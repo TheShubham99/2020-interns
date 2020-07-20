4a39f7398b8074a4b44fd14044382e12b880c9bc
Pratham <ssahasrabhojane@gmail.com>
2020-07-20 14:06:44 +0530

    feat: T2 Initial Implementation

--- T1.py
+++ T1.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 with open('./data.json') as f:
     data=json.load(f)
 
-ex_rate_jan=dict()
+ex_rate_inr_jan=dict()
+ex_rate_gbp_jan=dict()
+
 #iterate throgh all dates
 for i in data['rates']:
     date_obj = datetime.strptime(i, '%Y-%m-%d').date()
@@ -14,15 +16,21 @@ for i in data['rates']:
     #compare month and year (from 1 Jan 2019 to 31 Jan 2019)
     if(date_obj.month==1 and date_obj.year==2019):
         #create a dictionary for ploting
-        ex_rate_jan[date_obj.day]=data['rates'][str(i)]['INR']
+        ex_rate_inr_jan[date_obj.day]=data['rates'][str(i)]['INR']
+        ex_rate_gbp_jan[date_obj.day]=data['rates'][str(i)]['GBP']
 
 
 #Sort the data i.e. from 1st Jan to 31st Jan
-plot_list=sorted(ex_rate_jan.items())
+plot_list_inr=sorted(ex_rate_inr_jan.items())
+plot_list_gbp=sorted(ex_rate_gbp_jan.items())
+
 
 #prepare for ploting
-d, rate = zip(*plot_list)
+d, rate = zip(*plot_list_inr)
 plt.plot(d,rate)
+d, rate = zip(*plot_list_gbp)
+plt.plot(d,rate)
+
 
 #Labels
 plt.xlabel('Dates (from 1 Jan 2019 to 31 Jan 2019)')
